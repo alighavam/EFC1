45f13dbfeed163f0419041e73cd4a1430462e7ab
Ali Ghavampour <alighavam79@gmail.com>
2023-07-07 13:54:42 -0400

    Update efc1_RTvsRun.m
    Added average across subjects curve to the RTvsRun plot.

--- functions/efc1_RTvsRun.m
+++ functions/efc1_RTvsRun.m
@@ -18,15 +18,26 @@ figure
 colors = [[0 0.4470 0.7410];[0.8500 0.3250 0.0980];[0.9290 0.6940 0.1250];[0.4940 0.1840 0.5560];...
     [0.4660 0.6740 0.1880];[0.3010 0.7450 0.9330];[0.6350 0.0780 0.1840]];
 legNames = {};
+subjAvg = zeros(1,48);
 for i = 1:size(data,1)
     tmpData = data{i,1};
     idx = tmpData.trialPoint~=-1;
-    lineplot(tmpData.BN(idx),tmpData.RT(idx)-600,'plotfcn',plotfcn,...
-        'markercolor',colors(i,:),'linecolor',colors(i,:),'errorbars',{''});
+    [~,PLOT,~] = lineplot(tmpData.BN(idx),tmpData.RT(idx)-600,'plotfcn',plotfcn,...
+        'markertype','none','markercolor',colors(i,:),'linecolor',colors(i,:),'errorbars',{''});
+    subjAvg = subjAvg + PLOT/size(data,1);
+
     xlabel("Run Number")
-    ylabel(sprintf("%s RT(ms)",plotfcn))
+    ylabel(sprintf("%s ET(ms)",plotfcn))
     title("all subjects")
     hold on
     legNames{i} = data{i,2};
 end
+hold on
+plot(1:48,subjAvg,'k','LineWidth',6)
+
+legName{i+1} = 'Average';
 legend(legNames)
+
+
+
+
