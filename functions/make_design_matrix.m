e79f610824f7321ff78a7d57c1ca5850272a2dd4
Ali Ghavampour <53136447+alighavam@users.noreply.github.com>
2023-12-03 21:24:09 -0500

    Create make_design_matrix.m

---
+++ functions/make_design_matrix.m
@@ -0,0 +1,58 @@
+function X = make_design_matrix(chords,model_name)
+
+switch model_name
+    case 'num_fingers'
+        X = zeros(length(chords),5);
+        n = get_num_active_fingers(chords);
+        for i = 1:length(n)
+            X(i,n(i)) = 1;
+        end
+    
+    case 'extension'
+        X = zeros(length(chords),5);
+        for i = 1:length(chords)
+            X(i,:) = num2str(chords(i)) == '1';
+        end
+    
+    case 'single_finger'
+        X = zeros(length(chords),10);
+        for i = 1:length(chords)
+            X(i,1:5) = num2str(chords(i)) == '1';
+            X(i,6:10) = num2str(chords(i)) == '2';
+        end
+
+    case 'neighbour_fingers'
+        X = zeros(length(chords),16);
+        neighbour_chords = [11555,12555,21555,22555,...
+                   51155,51255,52155,52255,...
+                   55115,55125,55215,55225,...
+                   55511,55512,55521,55522];
+        for i = 1:length(chords)
+            for j = 1:length(neighbour_chords)
+                if (sum(num2str(chords(i)) == num2str(neighbour_chords(j))) == 2)
+                    X(i,j) = 1;
+                end
+            end
+        end
+
+    case 'two_finger_interactions'
+        X = [];
+        X1 = make_design_matrix(chords,'single_finger');
+        for j = 1:size(X1,2)-1
+            for k = j+1:size(X1,2)
+                if (k ~= j+5)
+                    X = [X, X1(:,j).*X1(:,k)]; 
+                end
+            end
+        end
+
+    otherwise
+        names = strsplit(model_name,'+');
+        if (length(names)<=1)
+            error('requested model name %s does not exist',model_name)
+        end
+        X = [];
+        for i = 1:length(names)
+            X = [X, make_design_matrix(chords,names{i})];
+        end
+end
