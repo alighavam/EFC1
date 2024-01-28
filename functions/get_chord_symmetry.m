7e277ff97c56d474e6aa720eabdc0fd89dbb5448
Ali Ghavampour <53136447+alighavam@users.noreply.github.com>
2024-01-28 14:06:24 -0500

    +all option

--- functions/get_chord_symmetry.m
+++ functions/get_chord_symmetry.m
@@ -29,7 +29,7 @@ for i = 1:length(chords)
 
         % flagging chords that are already made to avoid repetition:
         chords(chords==str2double(chord_vs)) = 0;
-
+        
     elseif strcmp(option,'horz')
         % skip flagged chords:
         if (chords(i) == 0)
@@ -52,6 +52,40 @@ for i = 1:length(chords)
         else
             continue;
         end
+
+    elseif strcmp(option,'all')
+        % skip flagged chords:
+        if (chords(i) == 0)
+            continue
+        end
+
+        % original chord:
+        C.chord(cnt,1) = chords(i);
+        
+        % transforming chord from num to str:
+        chord_str = int2str(chords(i));
+
+        % make vertical symmetry of chord i:
+        chord_vs = strrep(chord_str,'1','3');
+        chord_vs = strrep(chord_vs,'2','1');
+        chord_vs = strrep(chord_vs,'3','2');
+
+        % make horizontal symmetry of chord i:
+        chord_hs = flip(chord_str);
+
+        % make vertical horizontal symmetry of chord i:
+        chord_vhs = flip(chord_vs);
+
+        % store in dataframe:
+        C.chord_vs(cnt,1) = str2double(chord_vs);
+        C.chord_hs(cnt,1) = str2double(chord_hs);
+        C.chord_vhs(cnt,1) = str2double(chord_vhs);
+        cnt = cnt+1;
+    
+        % flagging chords that are already made to avoid repetition:
+        chords(chords==str2double(chord_vs)) = 0;
+        chords(chords==str2double(chord_hs)) = 0;
+        chords(chords==str2double(chord_vhs)) = 0;
     end
     
     % C.chord(cnt,1) = chords(i);
