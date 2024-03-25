05d171bd139843f1fd290e22760718b76f40794a
Ali Ghavampour <53136447+alighavam@users.noreply.github.com>
2024-03-24 21:38:14 -0400

    Update make_design_matrix.m

--- functions/make_design_matrix.m
+++ functions/make_design_matrix.m
@@ -1,4 +1,8 @@
 function X = make_design_matrix(chords,model_name)
+% setting paths:
+usr_path = userpath;
+usr_path = usr_path(1:end-17);
+project_path = fullfile(usr_path, 'Desktop', 'Projects', 'EFC1');
 
 switch model_name
     case 'n_fing'
@@ -56,6 +60,61 @@ switch model_name
             for k = j+1:size(X1,2)
                 if (k ~= j+5)
                     X = [X, X1(:,j).*X1(:,k)]; 
+                end
+            end
+        end
+
+    case ''
+
+    case 'force_avg'
+        data = dload(fullfile(project_path,'analysis','efc1_chord.tsv'));
+        sn_unique = unique(data.sn);
+        sess = [3,4];
+
+        % calculate group avg:
+        avg_force = 0;
+        for i = 1:length(sn_unique)
+            % getting the subject session average:
+            tmp_force = [];
+            for j = 1:length(sess)
+                diff_force = [data.force_f1(data.sn==sn_unique(i) & data.sess==sess(j)),...
+                              data.force_f2(data.sn==sn_unique(i) & data.sess==sess(j)),...
+                              data.force_f3(data.sn==sn_unique(i) & data.sess==sess(j)),...
+                              data.force_f4(data.sn==sn_unique(i) & data.sess==sess(j)),...
+                              data.force_f5(data.sn==sn_unique(i) & data.sess==sess(j))];
+                force = [diff_force,diff_force];
+                for col = 1:5
+                    force(force(:,col)<0,col) = 0;
+                    force(force(:,col+5)>0,col+5) = 0;
+                end
+                force(:,6:10) = force(:,6:10)*-1;
+
+                % if sum(isnan(force)) ~= 0
+                %     fprintf('subject %d, sess %d contains nan \n',sn_unique(i),sess(j))
+                % end
+
+                % tmp_force = tmp_force + force/length(sess);
+                tmp_force = cat(3,tmp_force,force);
+            end
+            tmp_force = sum(tmp_force,3,'omitnan')/length(sess);
+
+            avg_force = avg_force + tmp_force/length(sn_unique);
+        end
+        chordID = data.chordID(data.sn==1 & data.sess==4);
+        
+        % making the design matrix:
+        X = zeros(length(chords),10);
+        for i = 1:length(chords)
+            X(i,:) =  avg_force(chordID==chords(i),:);
+        end
+
+    case 'force_2fing'
+        X = [];
+        X1 = make_design_matrix(chords,'force_avg');
+        for j = 1:size(X1,2)-1
+            for k = j+1:size(X1,2)
+                if (k ~= j+5)
+                    X = [X, X1(:,j).*X1(:,k)];
                 end
             end
         end
