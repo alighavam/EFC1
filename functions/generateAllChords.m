17faefb2bbff23141533501c1c8df9d57231816b
Ali Ghavampour <53136447+alighavam@users.noreply.github.com>
2024-11-20 12:26:20 -0500

    clean up

--- functions/generateAllChords.m
+++ functions/generateAllChords.m
@@ -1,4 +1,4 @@
-function [chordVec] = generateAllChords()
+function [chordVec, chordVecSep] = generateAllChords()
 % Ali Ghavampour 2023 - alighavam79@gmail.com
 % This function generates all of the possible chords
 
@@ -43,4 +43,26 @@ for i = 1:size(chordMat,1)
     chordVec = [chordVec;tmp]; 
 end
 chordVec = chordVec(2:end); % chordVec contains all the possible unique chords other than 99999 (size:242x1) which is all fingers relaxed.
+
+
+% separating chords based on number of active fingers
+chordVecSep = cell(5,2);
+for i = 1:length(chordVec)
+    if (sum(num2str(chordVec(i))=='9')==4)
+        chordVecSep{1,1} = [chordVecSep{1,1} chordVec(i)];
+        chordVecSep{1,2} = [chordVecSep{1,2} i];
+    elseif (sum(num2str(chordVec(i))=='9')==3)
+        chordVecSep{2,1} = [chordVecSep{2,1} chordVec(i)];
+        chordVecSep{2,2} = [chordVecSep{2,2} i];
+    elseif (sum(num2str(chordVec(i))=='9')==2)
+        chordVecSep{3,1} = [chordVecSep{3,1} chordVec(i)];
+        chordVecSep{3,2} = [chordVecSep{3,2} i];
+    elseif (sum(num2str(chordVec(i))=='9')==1)
+        chordVecSep{4,1} = [chordVecSep{4,1} chordVec(i)];
+        chordVecSep{4,2} = [chordVecSep{4,2} i];
+    elseif (sum(num2str(chordVec(i))=='9')==0)
+        chordVecSep{5,1} = [chordVecSep{5,1} chordVec(i)];
+        chordVecSep{5,2} = [chordVecSep{5,2} i];
+    end
+end
 
